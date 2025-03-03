e237358772ac24bf08ca050ce8ac18cefb7b5860
Abhinav Dinesh <abhinavdinesh95@gmail.com>
2025-03-03 22:25:26 +0530

    feat: add darkmode bg gradient

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,14 +1,17 @@
 import { Box, Center, Heading } from "@chakra-ui/react";
-import { ColorModeButton } from "./ui/color-mode";
+import { ColorModeButton, useColorModeValue } from "./ui/color-mode";
 
 export default function HeroImage() {
+    const gradientFrom = useColorModeValue("red.200", "red.700");
+const gradientTo = useColorModeValue("blue.200", "blue.700");
   return (
     <Box
       position="relative"
       h="160px"
       bgGradient="to-r"
-      gradientFrom="red.200"
-      gradientTo="blue.200"
+      gradientFrom={gradientFrom}
+      gradientTo={gradientTo}
+
     >
       <Center h="100%">
         <Heading as="h1" fontSize={{ md: "6xl", sm: "4xl", base: "3xl" }}>
